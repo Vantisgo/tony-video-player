e0405f6d7c57d766d0adbcd5424a4faf12004591
Xanacas <53140620+Xanacas@users.noreply.github.com>
2025-12-20 10:57:42 +0100

    more progress

---
+++ prisma/prisma.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "../prisma/generated/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { env } from "~/env";
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+
+const adapter = new PrismaNeon({ connectionString: env.DATABASE_URL });
+
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+
+if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
